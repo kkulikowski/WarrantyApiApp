c88e99ce8ae080fe964a6f2c49debf9b025b86ca
kkulikowski <myownforge@gmail.com>
2017-08-28 23:35:16 +0200

    Add user model with tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824125424) do
+ActiveRecord::Schema.define(version: 20170828212946) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.string   "created_by"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "warranties", force: :cascade do |t|
